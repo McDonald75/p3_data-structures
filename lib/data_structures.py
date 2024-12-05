cd66157dfb787e09525d3e9cd611ba4cf569e253
McDonald75 <mcdonald.omondi@student.moringaschool.com>
2024-12-05 08:38:30 +0000

    complete

---
+++ lib/data_structures.py
@@ -0,0 +1,40 @@
+spicy_foods = [
+    {
+        "name": "Green Curry",
+        "cuisine": "Thai",
+        "heat_level": 9,
+    },
+    {
+        "name": "Buffalo Wings",
+        "cuisine": "American",
+        "heat_level": 3,
+    },
+    {
+        "name": "Mapo Tofu",
+        "cuisine": "Sichuan",
+        "heat_level": 6,
+    },
+]
+
+def get_names(spicy_foods):
+    return [food["name"] for food in spicy_foods]
+
+def get_spiciest_foods(spicy_foods):
+    return [food for food in spicy_foods if food["heat_level"]>5]
+
+def print_spicy_foods(spicy_foods):
+    return [print(f"{food["name"]} ({food["cuisine"]}) | Heat Level: {"🌶"*food["heat_level"]}") for food in spicy_foods]
+
+def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+    return next((food for food in spicy_foods if food["cuisine"] == cuisine), None)
+
+def print_spiciest_foods(spicy_foods):
+    return [print(f"{food["name"]} ({food["cuisine"]}) | Heat Level: {"🌶"*food["heat_level"]}") for food in spicy_foods if food["heat_level"]>5]
+
+def get_average_heat_level(spicy_foods):
+    return sum(food["heat_level"] for food in spicy_foods) / len(spicy_foods)
+
+def create_spicy_food(spicy_foods, spicy_food):
+    new_spicy_foods = spicy_foods
+    new_spicy_foods.append(spicy_food)
+    return new_spicy_foods
